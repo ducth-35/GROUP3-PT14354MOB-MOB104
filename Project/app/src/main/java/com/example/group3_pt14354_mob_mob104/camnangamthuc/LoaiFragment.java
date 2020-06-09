1135017c7396c534165a6000af2290760d93a71f
Trương Huỳnh Đức <ducthph07859@fpt.edu.vn>
2020-06-09 23:53:44 +0700

    thêm Fragment cho APP

---
+++ Project/app/src/main/java/com/example/group3_pt14354_mob_mob104/camnangamthuc/LoaiFragment.java
@@ -0,0 +1,13 @@
+package com.example.group3_pt14354_mob_mob104.camnangamthuc;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.group3_pt14354_mob_mob104.R;
+
+public class LoaiFragment extends Fragment {
+
+}
--- Project/app/src/main/java/com/example/group3_pt14354_mob_mob104/camnangamthuc/MainActivity.java
+++ Project/app/src/main/java/com/example/group3_pt14354_mob_mob104/camnangamthuc/MainActivity.java
@@ -1,7 +1,10 @@
 package com.example.group3_pt14354_mob_mob104.camnangamthuc;
 
+import android.support.design.widget.TabLayout;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 
 import com.example.group3_pt14354_mob_mob104.R;
 
@@ -11,5 +14,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
     }
 }
