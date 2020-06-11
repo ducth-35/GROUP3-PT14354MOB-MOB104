850c8524deb84bbdf837a450832d2fb590b1f1ce
Hiếu <54101547+xuanhieu147@users.noreply.github.com>
2020-06-11 22:35:49 +0700

    hieunxph07838_đã xong fragmentNoiBat

--- Project/app/src/main/java/com/example/group3_pt14354_mob_mob104/camnangamthuc/NoiBatFragment.java
+++ Project/app/src/main/java/com/example/group3_pt14354_mob_mob104/camnangamthuc/NoiBatFragment.java
@@ -1,64 +1,124 @@
 package com.example.group3_pt14354_mob_mob104.camnangamthuc;
 
+import android.app.AlertDialog;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ListView;
 
+import com.baoyz.widget.PullRefreshLayout;
 import com.example.group3_pt14354_mob_mob104.R;
+import com.example.group3_pt14354_mob_mob104.adapter.adapterMonAn;
+import com.example.group3_pt14354_mob_mob104.model.MonAn;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link NoiBatFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+
+import java.io.IOException;
+import java.util.ArrayList;
+
+import dmax.dialog.SpotsDialog;
+
+
 public class NoiBatFragment extends Fragment {
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
+    AlertDialog dialog;
+    String link = "http://www.doivi.net/nau-gi-hom-nay";
+    ListView lvNoiBat;
+    ArrayList<MonAn> listNoiBat;
+    adapterMonAn adapterNoiBat;
+    PullRefreshLayout layoutRefresh;
 
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
-
-    public NoiBatFragment() {
-        // Required empty public constructor
-    }
-
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment NoiBatFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static NoiBatFragment newInstance(String param1, String param2) {
-        NoiBatFragment fragment = new NoiBatFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
+    public NoiBatFragment() {}
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View view=inflater.inflate(R.layout.fragment_noi_bat, container, false);
+        layoutRefresh = (PullRefreshLayout)view.findViewById(R.id.swipeRefreshLayoutNoiBat);
+        return view;
     }
 
     @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
+    public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        lvNoiBat= (ListView) view.findViewById(R.id.lvNoiBat);
+        dialog = new SpotsDialog(getActivity(),R.style.Custom);
+        dialog.setCanceledOnTouchOutside(false);
+
+        layoutRefresh.setOnRefreshListener(new PullRefreshLayout.OnRefreshListener() {
+            @Override
+            public void onRefresh() {
+                listNoiBat.clear();
+                layoutRefresh.postDelayed(new Runnable() {
+                    @Override
+                    public void run() {
+                        new GetDataNoiBat().execute();
+                    }
+                },3000);
+            }
+        });
+        listNoiBat=new ArrayList<>();
+        new GetDataNoiBat().execute();
+
+
+    }
+
+    public class GetDataNoiBat extends AsyncTask<Void,Void,Void>
+    {
+        @Override
+        protected void onPostExecute(Void aVoid) {
+            super.onPostExecute(aVoid);
+            adapterNoiBat = new adapterMonAn(getActivity(), R.layout.item_monan,listNoiBat);
+            lvNoiBat.setAdapter(adapterNoiBat);
+            layoutRefresh.setRefreshing(false);
+        }
+
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+
+        }
+
+        @Override
+        protected Void doInBackground(Void... params) {
+            DocPageNoiBat();
+            return null;
         }
     }
-
-    @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_noi_bat, container, false);
+    public void DocPageNoiBat() {
+        Document doc = null;
+        try {
+            doc = Jsoup.connect(link).timeout(20*1000).get();
+            Log.d("KIEMTRA", "connect link thành công");
+        } catch (IOException e) {
+            e.getStackTrace();
+            Log.d("KIEMTRA","connect link lỗi");
+        }
+        Elements elements = doc.select("div.w-news-option li");
+        for (int i = 0; i < elements.size(); i++) {
+            Element e = elements.get(i);
+            Elements a = e.select("a");
+            String linkURL = a.attr("href");
+            Elements img = a.select("img");
+            String tenMon = img.attr("alt");
+            String LinkImage = img.attr("src");
+            Elements tit = e.select("div.w-news-option-item-name");
+            String title = tit.text();
+            MonAn monAn = new MonAn();
+            monAn.setTxtTenMon(tenMon);
+            monAn.setTitle(title);
+            monAn.setLinkImg(LinkImage);
+            monAn.setLinkURL("http://www.doivi.net" + linkURL);
+            if (LinkImage != "" && tenMon != "" && linkURL != "") {
+                listNoiBat.add(monAn);
+                Log.d("KIEMTRA","insert nổi bật: "+monAn.toString());
+            }
+        }
     }
 }
